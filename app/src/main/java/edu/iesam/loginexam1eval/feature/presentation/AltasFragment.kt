e331ff6dccbadcbcfbafc1ef022077f77c68253f
heennaarr <henar.herher@educa.jcyl.es>
2024-12-02 23:51:50 +0100

    ejercicio 1 terminado

--- app/src/main/java/edu/iesam/loginexam1eval/feature/presentation/AltasFragment.kt
+++ app/src/main/java/edu/iesam/loginexam1eval/feature/presentation/AltasFragment.kt
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import edu.iesam.loginexam1eval.databinding.FragmentAltasBinding
-import edu.iesam.loginexam1eval.feature.domain.User
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AltasFragment: Fragment() {
